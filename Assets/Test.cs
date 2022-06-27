9ee351b015d580c70507f5204e7531d84d9c41d7
Henry Shin <49927608+taeuk91@users.noreply.github.com>
2022-06-27 13:28:47 +0900

    update1

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,8 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.transform.position = Vector3.back;
+    
     }
 
     // Update is called once per frame
